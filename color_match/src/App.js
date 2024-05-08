867ea7c13722182f005da02e917a57db3805abfc
B1129023 <b1129023@cgu.edu.tw>
2024-05-08 21:12:33 +0800

    updated

---
+++ color_match/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Game from './Game';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Color Matching Game</h1>
+      </header>
+      <main>
+        <Game />
+      </main>
+    </div>
+  );
+}
+
+export default App;
